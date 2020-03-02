2021b1e1a61d601f8b6580f989dc06eef6b1ff27
tuxtron <32469400+tuxtron@users.noreply.github.com>
2020-03-02 14:54:23 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,13 +13,13 @@ sleep 60 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install spring-web/3.2.12.RELEASE_2 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin features:install spring-jdbc/3.2.12.RELEASE_2 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-api/3.9.3' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jdbc/3.9.1' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jta/3.9.1' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/atomikos-util/3.9.1' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-osgi/3.9.1' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jdbc/3.9.3' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-jta/3.9.3' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/atomikos-util/3.9.3' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.atomikos/transactions-osgi/3.9.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:commons-dbcp/commons-dbcp/1.4' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.springframework/spring-jdbc/2.5.5' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.mybatis/mybatis-spring/1.2.2' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.mybatis/mybatis-spring/1.2.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.google.guava/guava/19.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:commons-digester/commons-digester/1.8.1' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:commons-validator/commons-validator/1.5.1' && \
@@ -32,12 +32,24 @@ sleep 60 && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-core-asl/1.9.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-mapper-asl/1.9.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-jaxrs/1.9.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.codehaus.jackson/jackson-xc/1.9.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.fasterxml.jackson.jaxrs/jackson-jaxrs-json-provider/2.4.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.fasterxml.jackson.core/jackson-annotations/2.4.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.fasterxml.jackson.core/jackson-core/2.4.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.fasterxml.jackson.core/jackson-databind/2.4.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.fasterxml.jackson.jaxrs/jackson-jaxrs-base/2.4.0' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.cxf/cxf-rt-rs-security-cors/3.0.4' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.cxf/cxf-rt-frontend-jaxrs/3.0.4' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.bouncycastle/bcprov-jdk15on/1.53' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.bouncycastle/bcpkix-jdk15on/1.53' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.camel/camel-velocity/2.15.1' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s wrap:mvn:com.nimbusds/nimbus-jose-jwt/4.12' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.httpcomponents/httpcore-osgi/4.3.3' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.httpcomponents/httpclient-osgi/4.3.6' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.camel/camel-http4/2.15.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:org.apache.camel/camel-core/2.15.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:net.minidev/asm/1.0.2' && \
-/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:net.minidev/json-smart/2.1.1' && \
+/opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:net.minidev/json-smart/1.3.1' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'osgi:install -s mvn:com.jayway.jsonpath/json-path/1.2.0' && \
 /opt/jboss/jboss-full/bin/client -u admin -p admin 'shutdown -f' && \
 sleep 10
